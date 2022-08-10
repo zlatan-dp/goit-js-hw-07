f2fd67461ecfdd9cf15eb6fbc1878b129ad6d2f5
zlatan-dp <zlatandnepr@gmail.com>
2022-08-10 16:18:45 +0300

    gallery-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryImageItems = galleryItems
+  .map(
+    (galleryItem) => `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryContainer.innerHTML = galleryImageItems;
+
+galleryContainer.addEventListener("click", modalOpen);
+
+// function modalOpen(evt) {
+//   evt.preventDefault();
+//   if (!evt.target.dataset.source) return;
+//   basicLightbox
+//     .create(
+//       `
+//     <img src="${evt.target.dataset.source}">
+// `
+//     )
+//     .show();
+// }
+
+// document.addEventListener("keydown", (e) => {
+//   if (e.code !== "Escape") return;
+//   instance.close();
+// });
+
+function modalOpen(evt) {
+  evt.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`);
+
+  if (!evt.target.dataset.source) return;
+  instance.show();
+
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      if (e.code !== "Escape") return;
+      console.log(e.code);
+      instance.close();
+    },
+    { once: true }
+  );
+}
